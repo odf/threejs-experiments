a4668a1176f1acfe7c054db3ccf0628b359381e4
odf <olaf.delgado@gmail.com>
2012-06-27 00:49:38 +1000

    Ported most of the Javascript code to ClojureScript.

--- src-cljs/externs.js
+++ src-cljs/externs.js
@@ -1,3 +1,9 @@
 var THREE = {};
 
 THREE.PerspectiveCamera = function() {};
+THREE.WebGLRenderer = function() {};
+THREE.Scene = function() {};
+THREE.Mesh = function() {};
+THREE.SphereGeometry = function() {};
+THREE.MeshLambertMaterial = function() {};
+THREE.PointLight = function() {};
--- src-cljs/hello.cljs
+++ src-cljs/hello.cljs
@@ -2,10 +2,39 @@
   (:require [enfocus.core :as ef])
   (:require-macros [enfocus.macros :as em]))
 
-(def viewport (js-obj "width" 400 "height" 300))
-(def camera (js/camera viewport))
-(def scene (js/scene))
-(def renderer (js/renderer viewport))
+(defn light [{:keys [x y z]} color]
+  (doto (THREE.PointLight. color)
+    (-> .-position (.set x y z))))
+
+(defn lambert [color]
+  (THREE.MeshLambertMaterial. (js-obj "color" color)))
+
+(defn sphere [radius segments rings]
+  (THREE.SphereGeometry. radius segments rings))
+
+(defn mesh [geometrie {:keys [x y z]} material]
+  (doto (THREE.Mesh. geometrie material)
+    (-> .-position (.set x y z))))
+
+(def scene
+  (doto (THREE.Scene.)
+    (.add (mesh (sphere 50 16 16) { :x 0 :y 0 :z 0 } (lambert 0xCC2020)))
+    (.add (mesh (sphere 20 16 16) { :x 80 :y 50 :z 0 } (lambert 0xCCCCCC)))
+    (.add (light { :x 150 :y 300 :z 1000 } 0xFFFFFF))
+    (.add (light { :x -150 :y 300 :z -1000 } 0x8080FF))))
+
+(def viewport {:width 400 :height 300})
+
+(def camera
+  (let [view_angle 45
+        aspect (/ (:width viewport) (:height viewport))
+        near 0.1
+        far 10000]
+    (THREE.PerspectiveCamera. view_angle aspect near far)))
+
+(def renderer
+  (doto (THREE.WebGLRenderer.)
+    (.setSize (:width viewport) (:height viewport))))
 
 (.add scene camera)
 
