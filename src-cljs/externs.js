4e80e0c1f30c3fb9e3174e136680303eb62cf866
odf <olaf.delgado@gmail.com>
2012-06-27 15:18:31 +1000

    Added some methods to externs.js

--- src-cljs/externs.js
+++ src-cljs/externs.js
@@ -1,7 +1,12 @@
 var THREE = {};
 
-THREE.PerspectiveCamera = function() {};
 THREE.WebGLRenderer = function() {};
+THREE.WebGLRenderer.setSize = function() {};
+THREE.WebGLRenderer.render = function() {};
+
+THREE.PerspectiveCamera = function() {};
+THREE.PerspectiveCamera.lookAt = function() {};
+
 THREE.Scene = function() {};
 THREE.Mesh = function() {};
 THREE.SphereGeometry = function() {};
