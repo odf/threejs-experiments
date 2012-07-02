6db3e8c1b90a20bcae7c178c7fe9815185d7e50a
odf <olaf.delgado@gmail.com>
2012-07-02 23:22:16 +1000

    Preparations for generating ball-and-stick geometries.

--- src-cljs/hello.cljs
+++ src-cljs/hello.cljs
@@ -28,7 +28,40 @@
 (defn- mesh [geometrie position material]
   (doto (THREE.Mesh. geometrie material) (set-position! position)))
 
-(def viewport {:width 400 :height 300})
+(defn- map-values [f m]
+  (into {} (map (fn [[k v]] [k (f v)]) m)))
+
+(defn- dot [u v]
+  (into [] (apply + (map * u v))))
+
+(defn- cross [u v]
+  [(- (* (u 1) (v 2)) (* (u 2) (v 1)))
+   (- (* (u 2) (v 0)) (* (u 0) (v 2)))
+   (- (* (u 0) (v 1)) (* (u 1) (v 0)))])
+
+(defn- normalized [v]
+  (let [norm (Math/sqrt (dot v v))]
+    (into [] (map #(/ % norm) v))))
+
+(defn- make-stick
+  ([p q segments]
+     (let [d (normalized (map - q p))
+           u (cross d (if (> (dot d [1 0 0]) 0.9) [0 1 0] [1 0 0]))
+           v (cross d u)
+           a (/ (* 2 Math/PI) segment)
+           lc (fn [nu v mu w] (into [] (map #(+ (* nu %1) (* mu %2)) v w)))
+           section (map #(lc (Math/cos (* a %)) u (Math/sin (* a %)) v)
+                        (range segments))]))
+  ([p q]
+     (make-stick p q 8)))
+
+(defn- ball-and-stick [positions edges]
+  (let [ball-material (phong {:color 0xCC2020 :shininess 100})
+        balls (map-values #(mesh (sphere 10 8 8) % ball-material) positions)
+        sticks (map (fn [[u v]] (make-stick (positions u) (positions v))) edges)]
+    ))
+
+(def ^{:private true} viewport {:width 400 :height 300})
 
 (def ^{:private true} camera
   (let [view_angle 45
@@ -40,12 +73,18 @@
       (.lookAt (THREE.Vector3. 0 0 0))
       )))
 
+(def ^{:private true} test
+  (doto (mesh (THREE.CylinderGeometry. 5 5 50 8 1 false)
+              [60 0 0] (phong {:color 0x2020CC}))
+    (.lookAt (THREE.Vector3. 60 60 0))))
+
 (def ^{:private true} group
   (doto (THREE.Object3D.)
     (.add (mesh (sphere 50 16 16) [0 0 0]
                 (phong {:color 0xCC2020})))
     (.add (mesh (sphere 20 16 16) [80 50 0]
                 (phong {:color 0xCCCCCC :shininess 100})))
+    (.add test)
     ))
 
 (def ^{:private true} scene
