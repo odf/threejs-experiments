5878fadf741cf0ea8bb972baffea6fbc2d8ccbc8
odf <olaf.delgado@gmail.com>
2012-07-02 17:51:05 +1000

    Made all definitions private.

--- src-cljs/hello.cljs
+++ src-cljs/hello.cljs
@@ -2,35 +2,35 @@
   (:require [enfocus.core :as ef])
   (:require-macros [enfocus.macros :as em]))
 
-(defn js-map [cljmap]
+(defn- js-map [cljmap]
   (let [out js-obj]
     (doall (map #(aset out (name (first %)) (second %)) cljmap))
     out))
 
-(defn set-position! [obj [x y z]]
+(defn- set-position! [obj [x y z]]
   (-> obj .-position (.set x y z)))
 
-(defn set-rotation! [obj [x y z]]
+(defn- set-rotation! [obj [x y z]]
   (-> obj .-rotation (.set x y z)))
 
-(defn light [position color]
+(defn- light [position color]
   (doto (THREE.PointLight. color) (set-position! position)))
 
-(defn lambert [parameters]
+(defn- lambert [parameters]
   (THREE.MeshLambertMaterial. (js-map parameters)))
 
-(defn phong [parameters]
+(defn- phong [parameters]
   (THREE.MeshPhongMaterial. (js-map parameters)))
 
-(defn sphere [radius segments rings]
+(defn- sphere [radius segments rings]
   (THREE.SphereGeometry. radius segments rings))
 
-(defn mesh [geometrie position material]
+(defn- mesh [geometrie position material]
   (doto (THREE.Mesh. geometrie material) (set-position! position)))
 
 (def viewport {:width 400 :height 300})
 
-(def camera
+(def ^{:private true} camera
   (let [view_angle 45
         aspect (/ (:width viewport) (:height viewport))
         near 0.1
@@ -40,7 +40,7 @@
       (.lookAt (THREE.Vector3. 0 0 0))
       )))
 
-(def group
+(def ^{:private true} group
   (doto (THREE.Object3D.)
     (.add (mesh (sphere 50 16 16) [0 0 0]
                 (phong {:color 0xCC2020})))
@@ -48,25 +48,25 @@
                 (phong {:color 0xCCCCCC :shininess 100})))
     ))
 
-(def scene
+(def ^{:private true} scene
   (doto (THREE.Scene.)
     (.add group)
     (.add (light [150 300 1000] 0xFFFFFF))
     (.add (light [-150 300 -1000] 0x8080FF))
     (.add camera)))
 
-(def renderer
+(def ^{:private true} renderer
   (doto (THREE.WebGLRenderer.)
     (.setSize (:width viewport) (:height viewport))))
 
 (em/at js/document ["#container"] (em/append (.-domElement renderer)))
 
-(defn render []
+(defn- render []
   (let [timer (* (.now js/Date) 0.0001)]
     (set-rotation! group [0 timer 0])
     (.render renderer scene camera)))
 
-(defn animate []
+(defn- animate []
   (.requestAnimationFrame js/window animate)
   (render))
 
