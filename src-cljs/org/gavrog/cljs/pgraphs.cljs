3ccaf26b6cafcb8be93b425ebf6a3a4ae64cdacf
odf <olaf.delgado@gmail.com>
2012-08-03 14:26:17 +1000

    Implemented coordination sequence computation for periodic graphs.

--- src-cljs/org/gavrog/cljs/pgraphs.cljs
+++ src-cljs/org/gavrog/cljs/pgraphs.cljs
@@ -3,14 +3,11 @@
 ;; A periodic, undirected graph is given as a sequence of edges.
 ;;   
 ;; Edges look like (i,j,(a1,...,ak)), where
-;;   - i,j are natural numbers (excluding 0) representing vertices
+;;   - i,j are comparable vertex labels, usually positive integers
 ;;   - a1,...,ak are integers forming a vector in the integral lattice.
 ;;   
 ;; Conditions:
-;; - i <= j (forced direction)
-;; - special case: if i == j, then v must not be lexicographically smaller
-;;   than the zero vector
-;; - no edge may occur twice
+;; - no edge may occur twice with the same vector
 ;; - the resulting graph is connected
 ;; - the vectors all have the same dimension k and together span the
 ;;   k-dimensional lattice Z^k.
@@ -49,6 +46,11 @@
         [w s] (adjacent G v)
         :when (not (reverse-edge? v w s))]
     [v w s]))
+
+(defn neighbors [G v s]
+  (map (fn [[w t]] (vector w (map + s t))) (adjacent G v)))
+
+(defn origin [G] (repeat (dimension G) 0))
 
 (defrecord PGraph [dim verts adjs]
   IPGraph
@@ -89,3 +91,13 @@
   (reduce (fn [G [v w & s]] (with-edge G v w s))
           (PGraph. dim #{} {})
           (partition (+ 2 dim) edge-specs)))
+
+(defn coordination-sequence [graph seed]
+  (letfn [(step [previous current]
+            (let [advance #(into #{}
+                                 (for [[v s] current
+                                       x (neighbors graph v s)
+                                       :when (not (or (previous x) (current x)))]
+                                   x))]
+              (lazy-seq (cons (count current) (step current (advance))))))]
+    (step #{} #{[seed (origin graph)]})))
